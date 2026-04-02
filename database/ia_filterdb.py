018e29259005377305d0faccaf871c30a74a7361
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 11:50:31 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -2,7 +2,7 @@ import logging
 from struct import pack
 import re
 import base64
-from kurigram.file_id import FileId
+from pyrogram.file_id import FileId
 from typing import Dict, List
 from collections import defaultdict
 from pymongo.errors import DuplicateKeyError
